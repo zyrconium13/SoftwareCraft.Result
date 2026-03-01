0459ca68da310cb62dac455c4a51bf86ec9169d2
Eduard Popescu <peduard13@hotmail.com>
2026-03-01 13:50:54 +0200

    selectMany family variants for Task<Result<>>

--- SoftwareCraft.Result/TaskExtensions.cs
+++ SoftwareCraft.Result/TaskExtensions.cs
@@ -72,4 +72,72 @@ public static class TaskExtensions
     => await (await @this).SelectSwitchAsync(mapError);
 
   #endregion
+
+  #region SelectMany
+
+  public static async Task<Result<UValue, UError>> SelectMany<TValue, TError, UValue, UError>(
+    this Task<Result<TValue, TError>>    @this,
+    Func<TValue, Result<UValue, UError>> mapValue,
+    Func<TError, Result<UValue, UError>> mapError)
+    => (await @this).SelectMany(mapValue, mapError);
+
+  public static async Task<Result<UValue, TError>> SelectMany<TValue, TError, UValue>(
+    this Task<Result<TValue, TError>>    @this,
+    Func<TValue, Result<UValue, TError>> mapValue)
+    => (await @this).SelectMany(mapValue);
+
+  public static async Task<Result<TValue, UError>> SelectMany<TValue, TError, UError>(
+    this Task<Result<TValue, TError>>    @this,
+    Func<TError, Result<TValue, UError>> mapError)
+    => (await @this).SelectMany(mapError);
+
+  public static async Task<Result<UError>> SelectSwitchMany<TValue, TError, UError>(
+    this Task<Result<TValue, TError>> @this,
+    Func<TValue, Result<UError>>      mapValue,
+    Func<TError, Result<UError>>      mapError)
+    => (await @this).SelectSwitchMany(mapValue, mapError);
+
+  public static async Task<Result<TError>> SelectSwitchMany<TValue, TError>(
+    this Task<Result<TValue, TError>> @this,
+    Func<TValue, Result<TError>>      mapValue)
+    => (await @this).SelectSwitchMany(mapValue);
+
+  public static async Task<Result<UError>> SelectSwitchMany<TValue, TError, UError>(
+    this Task<Result<TValue, TError>> @this,
+    Func<TError, Result<UError>>      mapError)
+    => (await @this).SelectSwitchMany(mapError);
+
+  public static async Task<Result<UValue, UError>> SelectManyAsync<TValue, TError, UValue, UError>(
+    this Task<Result<TValue, TError>>          @this,
+    Func<TValue, Task<Result<UValue, UError>>> mapValue,
+    Func<TError, Task<Result<UValue, UError>>> mapError)
+    => await (await @this).SelectManyAsync(mapValue, mapError);
+
+  public static async Task<Result<UValue, TError>> SelectManyAsync<TValue, TError, UValue>(
+    this Task<Result<TValue, TError>>          @this,
+    Func<TValue, Task<Result<UValue, TError>>> mapValue)
+    => await (await @this).SelectManyAsync(mapValue);
+
+  public static async Task<Result<TValue, UError>> SelectManyAsync<TValue, TError, UError>(
+    this Task<Result<TValue, TError>>          @this,
+    Func<TError, Task<Result<TValue, UError>>> mapError)
+    => await (await @this).SelectManyAsync(mapError);
+
+  public static async Task<Result<UError>> SelectSwitchManyAsync<TValue, TError, UError>(
+    this Task<Result<TValue, TError>>  @this,
+    Func<TValue, Task<Result<UError>>> mapValue,
+    Func<TError, Task<Result<UError>>> mapError)
+    => await (await @this).SelectSwitchManyAsync(mapValue, mapError);
+
+  public static async Task<Result<TError>> SelectSwitchManyAsync<TValue, TError>(
+    this Task<Result<TValue, TError>>  @this,
+    Func<TValue, Task<Result<TError>>> mapValue)
+    => await (await @this).SelectSwitchManyAsync(mapValue);
+
+  public static async Task<Result<UError>> SelectSwitchManyAsync<TValue, TError, UError>(
+    this Task<Result<TValue, TError>>  @this,
+    Func<TError, Task<Result<UError>>> mapError)
+    => await (await @this).SelectSwitchManyAsync(mapError);
+
+  #endregion
 }
